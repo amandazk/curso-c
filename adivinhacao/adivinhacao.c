9d75757f8048ea73cc5734a05a66dd79108cf483
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-03 14:48:12 -0300

    Limitando os chutes

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -9,23 +9,27 @@ int main() {
 
     int numerosecreto = 42;
     int chute;
-    
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi: %d\n", chute);
 
-    if(chute == numerosecreto) {
-        printf("Você acertou!\n");
-    } 
-    else {
-        if(chute > numerosecreto) {
-            printf("Seu chute foi maior que o número secreto\n");
+    for (int i = 1; i <= 3; i++) {
+
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi: %d\n", chute);
+        
+        int acertou = chute == numerosecreto;
+
+        if(acertou) {
+            printf("Você acertou!\n");
+            break;
+        } 
+        else {
+            int maior = chute > numerosecreto;
+            if(maior) {
+                printf("Seu chute foi maior que o número secreto\n");
+            } else {
+                printf("Seu chute foi menor que o número secreto\n");
+            }
+            printf("Você errou!\n");
         }
-
-        if(chute < numerosecreto) {
-            printf("Seu chute foi menor que o número secreto\n");
-        }
-
-        printf("Você errou!\n");
     }
 }

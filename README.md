69aea71a3016c2e43e9d958b1997b838a273b22e
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-03 11:13:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Curso Linguagem C
+Aprendendo Linguagem C com exercícios
+
+## Como compilar um programa em C (Windows)
+- Na pasta onde se encontra o programa:
+```bash
+$ gcc adivinhacao.c -o adivinhacao.exe
+```
+## Rodando o programa
+```bash
+$ adivinhacao.exe
+```
---
+++ adivinhacao/adivinhacao.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    // Cabeçalho do jogo
+    printf("------------------------------------\n");
+    printf("Bem vindo(a) ao jogo de adivinhacao\n");
+    printf("------------------------------------\n");
+
+    int numerosecreto = 42;
+}

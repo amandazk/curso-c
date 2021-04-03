d5edf4c8c8512813fe16a2b7f935402ad452fbe9
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-03 13:34:47 -0300

    Chutando números

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -2,13 +2,15 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese_Brasil");
-
     // Cabeçalho do jogo
     printf("------------------------------------\n");
     printf("Bem vindo(a) ao jogo de adivinhação\n");
     printf("------------------------------------\n");
 
     int numerosecreto = 42;
-    // printf("O numero secreto eh o : %d\n", numerosecreto);
+    int chute;
+    
+    printf("Qual é o seu chute? ");
+    scanf("%d\n", &chute);
+    printf("Seu chute foi: %d\n", chute);
 }

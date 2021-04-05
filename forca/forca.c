ddb6aa0b0faaacc72aa807752f1c4623c667c03c
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-05 16:53:58 -0300

    Criando e imprimindo um array de caracteres

--- forca/forca.c
+++ forca/forca.c
@@ -3,4 +3,11 @@
 #include <time.h>
 
 int main() {
+
+    char palavrasecreta[20];
+
+    sprintf(palavrasecreta, "MELANCIA");
+
+    printf("%s", palavrasecreta); // imprime letra por letra
+    
 }

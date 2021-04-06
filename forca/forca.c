a4661730bcde688a15ff13c30459da2ab20c937c
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-06 15:25:53 -0300

    Laços encadeados

--- forca/forca.c
+++ forca/forca.c
@@ -8,17 +8,39 @@ int main() {
     sprintf(palavrasecreta, "MELANCIA");
 
     int acertou = 0;
-    int enforcou = 1;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
 
     do {
-        char chute;
-        scanf("%c", &chute);
 
-        for (int i = 0; i < strlen(palavrasecreta); i++) {
-            if (palavrasecreta[i] == chute) {
-                printf("A posição %d tem essa letra\n", i);
+        for(int i = 0; i < strlen(palavrasecreta); i++) {
+            
+            int achou = 0;
+
+            for(int j = 0; j < tentativas; j++) {
+                if(chutes[j] == palavrasecreta[i]) {
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if(achou) {
+                printf("%c ", palavrasecreta[i]);
+            } 
+            else {
+                printf("_ ");
             }
         }
+        printf("\n");
+
+        char chute;
+        printf("Qual letra? ");
+        scanf(" %c", &chute); // o espaço antes do %c serve para ignorar a tecla ENTER
+
+        chutes[tentativas] = chute;
+        tentativas++;
 
     } while (!acertou && !enforcou);
 }

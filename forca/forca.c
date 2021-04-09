c70d9f1224efb0ea86cd74ce1af818a25b3e4ce2
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-09 09:54:35 -0300

    Variáveis globais

--- forca/forca.c
+++ forca/forca.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+// variáveis globais
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
+
 void abertura() {
     
     printf("*******************\n");
@@ -8,17 +13,17 @@ void abertura() {
     printf("*******************\n\n");
 }
 
-void chuta(char chutes[26], int* tentativas) {
+void chuta() {
     
     char chute;
     printf("Qual letra? ");
     scanf(" %c", &chute);
 
-    chutes[(*tentativas)] = chute;
-    (*tentativas)++;
+    chutes[tentativas] = chute;
+    tentativas++;
 }
 
-int jachutou(char letra, char chutes[26], int tentativas) {
+int jachutou(char letra) {
 
     int achou = 0; // preciso do achou fora da função tambék
 
@@ -31,11 +36,11 @@ int jachutou(char letra, char chutes[26], int tentativas) {
 
     return achou;
 }
-void desenhaforca(char palavrasecreta[20], char chutes[26], int tentativas) {
+void desenhaforca() {
 
     for(int i = 0; i < strlen(palavrasecreta); i++) {
             
-        int achou = jachutou(palavrasecreta[i], chutes, tentativas);
+        int achou = jachutou(palavrasecreta[i]);
 
         if(achou) {
             printf("%c ", palavrasecreta[i]);
@@ -48,28 +53,23 @@ void desenhaforca(char palavrasecreta[20], char chutes[26], int tentativas) {
     printf("\n");
 }
 
-void escolhepalavra(char palavrasecreta[20]) {
+void escolhepalavra() {
 
     sprintf(palavrasecreta, "MELANCIA");
 }
 
 int main() {
 
-    char palavrasecreta[20];
-
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
-
-    escolhepalavra(palavrasecreta);
+    escolhepalavra();
     abertura();
 
     do {
 
-        desenhaforca(palavrasecreta, chutes, tentativas);
-        chuta(chutes, &tentativas);
+        desenhaforca();
+        chuta();
 
     } while (!acertou && !enforcou);
 }

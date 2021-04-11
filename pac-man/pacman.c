faadaee748c8da65cf5e4d126d5afe8e7e314666
Amanda Zacharuk <zacharukaz@gmail.com>
2021-04-11 17:28:28 -0300

    Salvando e lendo dados de uma matriz

--- pac-man/pacman.c
+++ pac-man/pacman.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-    char mapa[5][10]; // 5 linhas e 10 colunas
-    
+    char mapa[5][10+1]; // matriz de 5 linhas e 10 colunas
+
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+    if (f == 0) {
+        printf("Erro na leitura do mapa");
+        exit(1);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        fscanf(f, "%s", mapa[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%s\n", mapa[i]);
+    }
+
+    fclose(f);
+
 }
